8cc69a36e52e742a661e108613c1ab3cb3ae9ed2
franckDev21 <tutodevphp@gmail.com>
2021-10-06 01:13:58 +0100

    first

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <header class="header">
+    <div class="header_top">
+      <div class="header__logo">@success_dev</div>
+      <div class="widget"></div>
+    </div>
+    <div class="header_bottom">
+      <div class="header__nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+      <div class="header__network"></div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+}
+</script>
+
+<style lang="scss">
+  .header{
+    padding: 15px 10px;
+    background-color: #1e1f1e;
+    color: #fff;
+    &__top,&__bottom{
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+    a.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button v-on:click="show = !show">
+      Toggle
+    </button>
+    <transition name="fade">
+      <p v-if="show">
+        <hello-world></hello-world>
+      </p>
+    </transition>
   </div>
 </template>
 
@@ -11,8 +18,22 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
+  data(){
+    return {
+      show : true
+    }
+  },
   components: {
     HelloWorld
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+  }
+  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+  }
+</style>
